723974e9283a211ecf2436873a6f882dda270f32
PatrickFavret <110774860+PatrickFavret@users.noreply.github.com>
2024-08-05 21:09:44 -0400

    Add function that reverses a string
    I used a for loop to get each letter of the input string from back to front and concactenated with a new string that is returned at the end.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(stringToReverse) {
+    reversedString = "";
+    for (let i = stringToReverse.length-1; i > -1; i--) {
+        reversedString += stringToReverse[i];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
