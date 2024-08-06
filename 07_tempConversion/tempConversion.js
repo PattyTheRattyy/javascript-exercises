b188a9c57c03cc149928c7c5b54d94fd424e9a2b
PatrickFavret <110774860+PatrickFavret@users.noreply.github.com>
2024-08-06 19:31:38 -0400

    Add function to convert between celsius and fahrenheit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  console.log(`Fahrenheit value: ${fahrenheit}`);
+
+  let converted = ((fahrenheit - 32) * (5/9)).toFixed(1);
+
+  console.log(`Converted value ${converted}`);
+  return parseFloat(converted);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  console.log(`Celsius value: ${celsius}`);
+
+  let converted = ((celsius * (9/5) + 32)).toFixed(1);
+
+  console.log(`Converted value ${converted}`);
+  return parseFloat(converted);
 };
 
 // Do not edit below this line
