2f3688fc4bd44d6559d58abbea0b5f03fb96aa0e
PatrickFavret <110774860+PatrickFavret@users.noreply.github.com>
2024-08-06 18:12:22 -0400

    Add function that sums every number between two integers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num1) || typeof(num1) !== "number" || typeof(num2) !== "number") {
+        return "ERROR";
+    }
+
+    let smaller = num1
+    let bigger = num2;
+
+    if (smaller > bigger) {
+        bigger = num1;
+        smaller = num2;
+    }
+
+    let sum = 0;
+
+    for (let i = smaller; i < bigger+1; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
