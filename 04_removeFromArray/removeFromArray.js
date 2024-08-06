d69f26db39b494da416135c04c1fc364b76db8db
PatrickFavret <110774860+PatrickFavret@users.noreply.github.com>
2024-08-06 17:47:59 -0400

    Add solution function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+
+
+const removeFromArray = function(array, ...nums) {
+
+    let arrayToReturn = [];
+
+    array.forEach((num) => {
+        if (!nums.includes(num)){
+            arrayToReturn.push(num)
+        }
+    });
+    return arrayToReturn;
 };
 
+  
 // Do not edit below this line
 module.exports = removeFromArray;
