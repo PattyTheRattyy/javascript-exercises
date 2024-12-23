2d3a28f68b16a48e729ca59a30a4fb914cea9f6f
PatrickFavret <110774860+PatrickFavret@users.noreply.github.com>
2024-12-23 15:53:39 -0500

    Finish palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.toLowerCase();
+    rightIndex = word.at(-1);
+    for (leftIndex = 0; i < word.length; i++) {
+        if (word.at(leftIndex) != word.at(rightIndex)) {
+            return false;
+        }
+        rightIndex--;
+    }
+    return true;
 };
 
 // Do not edit below this line
