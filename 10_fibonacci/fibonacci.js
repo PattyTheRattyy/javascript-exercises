936d105b75ee030944aaf0a838f813dac0a504a1
PatrickFavret <110774860+PatrickFavret@users.noreply.github.com>
2024-12-23 20:11:00 -0500

    Finish fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
+function fibonacciFunc(memberNum) {
+    if (memberNum === 0){
+        return [0];
+    } else if (memberNum === 1){
+        return [0,1];
+    }
+    return fibonacciFunc(memberNum-1) + fibonacciFunc(memberNum-2);
 };
+const fibonacci = function(memberNum) {
+    fibArray = [];
+    for (i = 2; i < memberNum; i++){
+        fibArray.push(fibonacciFunc(i));
+    }
+    return fibArray[memberNum-1];
+}
+
 
 // Do not edit below this line
 module.exports = fibonacci;
